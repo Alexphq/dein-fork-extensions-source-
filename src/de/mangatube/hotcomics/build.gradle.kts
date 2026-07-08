f9e1f63272b2d29e989a99b5ce36782fc631f3d2
Alexphq <108958044+Alexphq@users.noreply.github.com>
2026-07-08 18:45:35 +0200

    Update build.gradle.kts

--- src/de/mangatube/hotcomics/build.gradle.kts
+++ src/de/mangatube/hotcomics/build.gradle.kts
@@ -1,1 +1,14 @@
+plugins {
+    alias(kei.plugins.extension)
+}
 
+keiyoushi {
+    name = "HotComics.io"
+    versionCode = 1
+    contentWarning = ContentWarning.NSFW
+    libVersion = "1.4"
+    source {
+        lang = "de"
+        baseUrl = "https://hotcomics.io"
+    }
+}
